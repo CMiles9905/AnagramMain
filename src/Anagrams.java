7ec7adbd11046d00acf50bfb7456fb7575856b5c
Chase <CMiles9905@student.whatcom.edu>
2020-03-13 21:32:31 -0700

    Version 1

---
+++ src/Anagrams.java
@@ -0,0 +1,27 @@
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.*;
+public class Anagrams {
+
+
+    public Anagrams(Set<String> dictionary){
+    
+    }
+
+    public void print (String phrase){
+    }
+
+    public Set<String> getWords(String phrase){
+    LetterInventory s = new LetterInventory(phrase);
+
+    }
+
+
+
+    public void print(String phrase, int max){
+
+    }
+
+}
+
+
